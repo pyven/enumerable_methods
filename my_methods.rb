b2e2deab270f8d0553fa4e5c8f22d8dec730715c
pyven <quimegson@gmail.com>
2020-04-17 21:30:20 +0800

    adds my_select through my_map methods

--- my_methods.rb
+++ my_methods.rb
@@ -1,5 +1,7 @@
 module Enumerable
     def my_each
+        return self.to_enum unless block_given?
+
         i = 0
         loop do
             yield(self[i])
@@ -9,6 +11,8 @@ module Enumerable
     end
 
     def my_each_with_index
+        return self.to_enum unless block_given?
+
         i = 0
         loop do 
             yield(self[i], i)
@@ -17,4 +21,55 @@ module Enumerable
         end
     end
 
-    
+    def my_select
+        return self.to_enum unless block_given?
+
+        accepted = []
+        self.my_each do |i|
+            accepted.push(i) if yield(i)
+        end
+        accepted
+    end
+
+    def my_all?
+        validator = true
+        self.my_each { |i| validator = false if !yield(i) }
+        validator
+    end
+
+    def my_any?
+        validator = false
+        self.my_each { |i| validator = true if yield(i) }
+        validator
+    end
+
+    def my_none?
+        validator = true
+        self.my_each { |i| validator = false if yield(i) }
+    end
+
+    def my_count(arg)
+        return self.length unless block_given? && arg.nil?
+
+        count = 0
+        if block_given?
+            self.my_each { |i| counter += 1 if yield[i] }
+        elsif !arg.nill
+            self.my_each { |i| counter += 1 if yield[i] == i}
+        end
+        count
+    end
+
+    def my_map
+        return self.to_enum unless block_given?
+
+        result =[]
+        self.my_each { |i| result.push(yield) }
+        result
+    end
+
+        
+        
+end
+
+
